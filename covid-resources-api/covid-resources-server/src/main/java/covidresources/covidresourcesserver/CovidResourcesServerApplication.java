005a7323694289b36de3051791a49a0b2369db7a
Ashwini Tomar <ashwini.tomar@mykaarma.com>
2021-04-24 23:57:10 +0530

    initial commit

---
+++ covid-resources-api/covid-resources-server/src/main/java/covidresources/covidresourcesserver/CovidResourcesServerApplication.java
@@ -0,0 +1,13 @@
+package covidresources.covidresourcesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidResourcesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidResourcesServerApplication.class, args);
+	}
+
+}
