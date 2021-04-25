337d2756696256cc057a146b0ea809aa551d6d1d
rahul yadav <>
2021-04-25 16:31:49 +0530

    updated key

--- covid-resources-api/covid-resources-server/src/main/java/covidresources/LeadsRepository.java
+++ covid-resources-api/covid-resources-server/src/main/java/covidresources/LeadsRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 public class LeadsRepository extends AbstractFirestoreRepository<Lead> {
 
     protected LeadsRepository(Firestore firestore) {
-        super(firestore, "test");
+        super(firestore, "Lead");
     }
 
 
